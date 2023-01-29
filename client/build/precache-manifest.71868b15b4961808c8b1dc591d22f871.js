e2b5424ceb26f59c1d6c968dc13e003fa63e21ea
Nandhiniumesh <nandhiniumesh@gmail.com>
2023-01-29 19:25:16 +0530

    first commit

---
+++ client/build/precache-manifest.71868b15b4961808c8b1dc591d22f871.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c3d676e05985211bdcf1c8a9d7a93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78099ca7e980ad7d42da",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "5619efc5dfdd1a1d98e7",
+    "url": "/static/css/main.2be03680.chunk.css"
+  },
+  {
+    "revision": "78099ca7e980ad7d42da",
+    "url": "/static/js/2.37b68800.chunk.js"
+  },
+  {
+    "revision": "2a0b20e1d02e77e8b582ff21a215748a",
+    "url": "/static/js/2.37b68800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5619efc5dfdd1a1d98e7",
+    "url": "/static/js/main.3ae7fca8.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "90dabe9344551a1f7385104e2454b1e9",
+    "url": "/static/media/404.90dabe93.jpg"
+  },
+  {
+    "revision": "4c4dc7719477c9c6d48ba14c5d04af53",
+    "url": "/static/media/Mihir.4c4dc771.jpeg"
+  },
+  {
+    "revision": "42d709e436404ca5c9cad3c6de0c31d6",
+    "url": "/static/media/check.42d709e4.png"
+  },
+  {
+    "revision": "837e0b22921e25f5a2eca9d514a4a2e1",
+    "url": "/static/media/close.837e0b22.png"
+  },
+  {
+    "revision": "e295470bc88c8d972e83f4e3d77f7ea9",
+    "url": "/static/media/empty.e295470b.png"
+  },
+  {
+    "revision": "2f55debd98b4040009b3ab545969577d",
+    "url": "/static/media/icons8-github.2f55debd.svg"
+  },
+  {
+    "revision": "304fb3151e3b1f86d0150dc8144fcc3c",
+    "url": "/static/media/icons8-instagram.304fb315.svg"
+  },
+  {
+    "revision": "2c20928ddc18c3e2ac70e6f8f38ca825",
+    "url": "/static/media/icons8-linkedin.2c20928d.svg"
+  },
+  {
+    "revision": "6293f16266f118ba96ae833d911701db",
+    "url": "/static/media/logo.6293f162.svg"
+  },
+  {
+    "revision": "a8f6abbb4ea76be277e34cbb17ca9175",
+    "url": "/static/media/noItem.a8f6abbb.png"
+  },
+  {
+    "revision": "bc72b72ec06fc6435aad6be9949ed8df",
+    "url": "/static/media/register.bc72b72e.svg"
+  }
+]);
